8f0ec6e0b7b21804841aa80c7b92c4b4ec9bfbad
Catherine Stewart <catherine.c.stwrt@gmail.com>
2021-05-28 09:41:56 -0700

    separate data in vs. data out; expand takes context instead of target only

--- writmacs/expand.py
+++ writmacs/expand.py
@@ -20,6 +20,7 @@ from .parse import parse
 from .macros import expanders, organizers, contextualizers
 from .util import TARGETS, Node
 
+DEFAULT_CONTEXT = {'target': 'md'}
 
 def AST2tree(syntax_node):
     name = syntax_node['name']
@@ -49,60 +50,76 @@ def semantic_tree(macs_txt):
     return AST2tree(AST)
 
 
-def eval_forest(forest, metadata=None):
+def eval_forest(forest, context=None):
 
-    metadata = metadata if metadata is not None else {'target': 'md'}
+    if context is None:
+        context = {}
+
+    data_out = {}
     builder = []
 
     for item in forest:
         if type(item) is Node:
-            chunks, inner_data = eval_tree(item, metadata, collapse=False)
+            chunks, tree_data_out = eval_tree(item, context)
             builder.extend(chunks)
-            metadata = {**metadata, **inner_data} # later > earlier
+            data_out = {**data_out, **tree_data_out} # later > earlier
         else:
+            # a str or something that can be cast to one e.g. a Token
             builder.append(item)
 
-    return builder, metadata
+    return builder, data_out
 
 
-def eval_tree(mac_tree, metadata=None, collapse=True):
+def eval_tree(mac_tree, context=None):
 
-    metadata = metadata if metadata is not None else {'target': 'md'}
+    if context is None:
+        context = {}
+
+    # create context specifically for this sub-tree
+    if mac_tree.name in contextualizers:
+        # deeper > shallower
+        local_context = {
+            **context,
+            **contextualizers[mac_tree.name](mac_tree.children)
+        }
+    else:
+        local_context = context
+
+    # evaluate the sub-tree below
     children = []
+    data_out = {}
+    for forest in mac_tree.children:
+        builder, child_data_out = eval_forest(forest, local_context)
+        children.append(builder)
+        data_out.update(child_data_out)
 
-    for nth, forest in enumerate(mac_tree.children):
-        if mac_tree.name in contextualizers:
-            # deeper > shallower
-            local_metadata = {**metadata, **contextualizers[mac_tree.name][nth]}
-        else:
-            local_metadata = metadata
-        chunks, inner_data = eval_forest(forest, local_metadata)
-        children.append(chunks)
-        metadata = {**inner_data, **metadata} # shallower > deeper
-
+    # perform the macro operation for this tree node
     if mac_tree.name in expanders:
-        builder, *more_data = expanders[mac_tree.name](
-                children, metadata
+        builder_out, *more_data = expanders[mac_tree.name](
+                children, context
                 )
         if len(more_data) == 1:
-            metadata = {**more_data[0], **metadata}
+            data_out.update(more_data[0])
     else:
-        builder = [chunk for child in children for chunk in child]
-    if collapse:
-        return ''.join([str(chunk) for chunk in builder]), metadata
-    else:
-        return builder, metadata
+        # if no operation to do, default to flattening children together
+        builder_out = [chunk for child in children for chunk in child]
 
-def expand(main_txt, target='md'):
+    return builder_out, data_out
+
+
+def expand(main_txt, context=None):
+    if context is None:
+        context = DEFAULT_CONTEXT
     main_tree = semantic_tree(main_txt)
-    return eval_tree(main_tree, {'target': target})
+    full_builder, full_meta = eval_tree(main_tree, context)
+    return ''.join([str(chunk) for chunk in full_builder]), full_meta
 
 if __name__ == '__main__':
     mac_txt = sys.stdin.read()
     target = sys.argv[1] if len(sys.argv) > 1 else None
     if target not in TARGETS:
         target = 'md'
-    result, metadata = expand(mac_txt)
+    result, metadata = expand(mac_txt, {'target': target})
 
     print(result)
 
